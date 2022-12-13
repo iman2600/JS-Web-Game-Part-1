12df7fb74f602e57ee7f822bd8fcc14e54fbfb7b
Isaac Sirstins <113957847+iman2600@users.noreply.github.com>
2022-12-12 21:52:44 -0700

    Finished Assignment

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(path, left, bottom) {
+  let object = document.createElement("img");
+  object.src = path;
+  object.style.position = "fixed";
+  object.style.left = left + "px";
+  object.style.bottom = bottom + "px";
+  document.body.append(object);
+  return object;
+}
+
+function newItem(path, left, bottom) {
+  let object = newImage(path, left, bottom);
+  object.addEventListener("dblclick", function () {
+    object.remove();
+  });
+}
+
+newImage("assets/green-character.gif", 100, 100);
+newImage("assets/pine-tree.png", 450, 200);
+newImage("assets/tree.png", 200, 300);
+newImage("assets/crate.png", 150, 200);
+newImage("assets/well.png", 500, 425);
+
+newItem("assets/sword.png", 500, 405);
+newItem("assets/sheild.png", 165, 185);
+newItem("assets/staff.png", 600, 100);
